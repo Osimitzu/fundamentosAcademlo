034162bd75c1bf019a19e33c669372d19aa43b41
Osimitzu <angel_aocc@hotmail.com>
2021-12-23 17:45:11 -0600

    finalClaseDic-2

--- dic-2/js/main.js
+++ dic-2/js/main.js
@@ -1,4 +1,6 @@
 
+"use strict";
+
 /*
 Declaración de una función: 
 function nombreDeLaFuncion (params) {
@@ -98,6 +100,85 @@ obtenerInformación ( (nombre, edad) => console.log (`Hola me llamo ${nombre} y 
 
 
 
+            //FUNCIÓN AUTOINVOCADA
+//Es invocada automaticamente sin ser llamada
+//Antes de tener una de estas funciones forsozamente la linea previa debe de tener un punto y coma; (aplica lo mismo para instrucciones que comienzan con parentesis, corchetes o llaves)
+/* 
+;( function (params) {
+    //Instrucciones
+    return
+} )(); 
+*/
+/*
+let variable = "algo";
+;(function(){
+    alert ("¡Hola Mundo!")
+})();
+*/
+
+
+
+/*
+            //CLOSURES
+function creaSumador(x) {
+  return function(y) {
+    return x + y;
+  };
+}
+var suma5 = creaSumador(5);
+console.log (suma5);
+console.log(suma5(2));  // muestra 7
+*/
+
+
+
+
+                //FUNCIÓNES DE ORDEN SUPERIOR 
+//Funciones que aceptan otras funciones como parametros o retornan funciones
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//forEach
+/*let resultado = numbers.forEach( ( elemento, indice, array ) => {
+    console.log (`El elemento ${elemento} se encuentra en el indice ${indice} del arreglo ${array}`);
+    return elemento*2; //Aunque se ponga un return forEach NO retorna NADA es undefined
+});
+console.log (resultado); // Devuelve undefined*/
+
+//map
+/*let resultadoMap = numbers.map ( ( elemento, indice, array ) => {
+    console.log (`El elemento ${elemento} se encuentra en el indice ${indice} del arreglo ${array}`);
+    return elemento*2;
+} );
+console.log (resultadoMap); // Devuelve el return (elemento*2)*/
+let pares = [];
+let resultado = numbers.forEach( ( elemento, indice, array ) => {
+    if (elemento % 2 === 0) pares.push (elemento);
+} )
+console.log (resultado);
+console.log (pares);
+
+
+let resultadoMap = numbers.map ( ( elemento, indice, array ) => {
+    if (elemento % 2 === 0) return elemento;
+} );
+console.log (resultadoMap);
+
+//filter
+/*let resultadoFilter = numbers.filter ( ( elemento, indice, array) => {
+    return (elemento % 2) === 0
+} );*/
+// Lo mismo que arriba pero simplificado con return Implicito:
+let resultadoFilter = numbers.filter ( ( elemento, indice, array) => (elemento % 2) === 0 ); 
+console.log (resultadoFilter);
+
+
+//reduce
+let resultadoReduce = numbers.reduce ( (acumulador, valorActual, indice, array) => {
+    console.log (acumulador, valorActual);
+    return acumulador + valorActual;
+}, 0 );
+// Lo mismo que arriba pero simplificado con return Implicito:
+//let resultadoReduce = numbers.reduce ( (acumulador, valorActual, indice, array) => acumulador + valorActual, 0 );
+console.log (resultadoReduce);
 
 
 
