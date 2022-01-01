13853a327046eba9eb6be0c2d0c9e55ab5f05f16
Osimitzu <angel_aocc@hotmail.com>
2021-12-31 19:00:39 -0600

    dic-3

---
+++ dic-3/js/main.js
@@ -0,0 +1,54 @@
+
+"use strict";
+console.log ("HolaPuto");
+
+/*
+    const listaProductos = [
+    {
+        precio: 20,
+        title: "Helado",
+        imgURL: "https://picsum.photos/200/300"
+    },
+    {
+        precio: 15,
+        title: "Chocolate",
+        imgURL: "https://picsum.photos/200/300"
+    },
+    {
+        precio: 150,
+        title: "Pizza",
+        imgURL: "https://picsum.photos/200/300"
+    },
+    {
+        precio: 75,
+        title: "Yogurth",
+        imgURL: "https://picsum.photos/200/300"
+    },
+    {
+        precio: 35,
+        title: "Caramelos",
+        imgURL: "https://picsum.photos/200/300"
+    },
+    {
+        precio: 100,
+        title: "Vino",
+        imgURL: "https://picsum.photos/200/300"
+    },
+    {
+        precio: 750,
+        title: "Caja de dulces",
+        imgURL: "https://picsum.photos/200/300"
+    },
+    {
+        precio: 40,
+        title: "Pan integral",
+        imgURL: "https://picsum.photos/200/300"
+    },
+    {
+        precio: 200,
+        title: "Pastel",
+        imgURL: "https://picsum.photos/200/300"
+    }
+]
+*/
+
